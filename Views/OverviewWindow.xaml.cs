d6627024b7187aea24aa7d08bbe8eb4523c92527
Jake Gough <super.jake@hotmail.co.uk>
2018-05-08 10:32:16 +0100

    Update OverviewWindow.xaml.cs

--- Views/OverviewWindow.xaml.cs
+++ Views/OverviewWindow.xaml.cs
@@ -1,21 +1,44 @@
-﻿using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows;
+using ERSApp.Model;
 using ERSApp.ViewModel;
 using MahApps.Metro.Controls;
+using System.Windows.Controls;
 
 namespace ERSApp.Views
 {
     public partial class OverviewWindow : MetroWindow
     {
+        public ObservableCollection<Session> OverviewSessions { get; set; }
+
         public OverviewWindow()
         {
             InitializeComponent();
-            this.DataContext = new SessionViewModel();
+            this.DataContext = this;
             tabOverview.Header = CollectionManager.SelectedDate.ToShortDateString();
+            OverviewSessions = new ObservableCollection<Session>(SessionViewModel.Sessions);
+            foreach(Session s in OverviewSessions)
+            {
+                cboLocation.Items.Add(s.Location);
+            }
+        }
+
+        private void cboLocation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OverviewSessions.Clear();
+            foreach(Session s in SessionViewModel.Sessions)
+            {
+                if (s.Location.Equals(cboLocation.SelectedValue))
+                {
+                    OverviewSessions.Add(s);
+                }
+            }
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
     }
 }
