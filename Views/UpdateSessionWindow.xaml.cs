2a87e4741738e45563afc94307d64dc89ef02e6e
Jake Gough <super.jake@hotmail.co.uk>
2018-05-10 13:07:57 +0100

    Update UpdateSessionWindow.xaml.cs

--- Views/UpdateSessionWindow.xaml.cs
+++ Views/UpdateSessionWindow.xaml.cs
@@ -1,9 +1,10 @@
-using System;
 using System.Windows;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using ERSApp.Model;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
 
 namespace ERSApp.Views
 {
@@ -16,30 +17,21 @@ namespace ERSApp.Views
             InitializeComponent();
             this.Selected = s;
             txtDate.Text = Selected.Date;
-            cboStartTime.Items.Add("08:00");
-            cboStartTime.Items.Add("09:00");
-            cboStartTime.Items.Add("10:00");
-            cboStartTime.Items.Add("11:00");
-            cboStartTime.Items.Add("12:00");
-            cboEndTime.Items.Add("14:00");
-            cboEndTime.Items.Add("15:00");
-            cboEndTime.Items.Add("16:00");
-            cboEndTime.Items.Add("17:00");
-            cboEndTime.Items.Add("18:00");
             txtLocation.Text = Selected.Location;
-            cboMDC.Items.Add("No");
-            cboMDC.Items.Add("Yes");
-            cboStartTime.SelectedItem = Selected.StartTime;
-            cboEndTime.SelectedItem = Selected.EndTime;
-            cboMDC.SelectedItem = Selected.MDC;
-            PopulateChairs(cboMDC.Text);
+            txtClinicTime.Text = Selected.ClinicTime;
+            txtLOD.Text = Selected.LOD.ToString();
+            cboType.Items.Add("Community");
+            cboType.Items.Add("MDC");
+            cboType.SelectedItem = Selected.Type;
+            PopulateChairs(cboType.Text);
             cboChairs.SelectedItem = Selected.Chairs.ToString();
+            txtBleeds.Text = Selected.Bleeds.ToString();
         }
 
-        public void PopulateChairs(string MDC)
+        public void PopulateChairs(string type)
         {
             cboChairs.Items.Clear();
-            if (MDC.Equals("Yes"))
+            if (type.Equals("MDC"))
             {
                 cboChairs.Items.Add("3");
                 cboChairs.Items.Add("6");
@@ -54,52 +46,50 @@ namespace ERSApp.Views
             }
         }
 
-        private void cboMDC_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void cboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (cboMDC.SelectedIndex == 0)
+            if (cboType.SelectedIndex == 0)
             {
-                PopulateChairs("No");
+                PopulateChairs("Community");
             }
             else
             {
-                PopulateChairs("Yes");
+                PopulateChairs("MDC");
             }
         }
 
         private async void btnUpdateSession_Click(object sender, RoutedEventArgs e)
         {
             //Makes sure each input has an input before being made. Possibly better way to do this but this works and is simple to update
-            if(Selected.SV1Id == 0 && Selected.DRI1Id == 0 && Selected.DRI2Id == 0 && Selected.RN1Id == 0 && Selected.RN2Id == 0
+            if (Selected.SV1Id == 0 && Selected.DRI1Id == 0 && Selected.DRI2Id == 0 && Selected.RN1Id == 0 && Selected.RN2Id == 0
                 && Selected.RN3Id == 0 && Selected.CCA1Id == 0 && Selected.CCA2Id == 0 && Selected.CCA3Id == 0)
             {
-                if (cboStartTime.Text == "")
+                if (txtLOD.Text == "")
                 {
-                    await this.ShowMessageAsync("", "Please select a Start Time.");
+                    await this.ShowMessageAsync("", "Please select a Length Of Day.");
                 }
-                else if (cboEndTime.Text == "")
+                else if (double.Parse(txtLOD.Text) < 0)
                 {
-                    await this.ShowMessageAsync("", "Please select an End Time.");
+                    await this.ShowMessageAsync("", "Please select a valid Length Of Day.");
                 }
-                else if (txtLocation.Text == "")
+                else if (cboType.Text == "")
                 {
-                    await this.ShowMessageAsync("", "Please enter a Location.");
-                }
-                else if (cboMDC.Text == "")
-                {
-                    await this.ShowMessageAsync("", "Please select if session is MDC or not.");
+                    await this.ShowMessageAsync("", "Please select a Session Type.");
                 }
                 else if (cboChairs.Text == "")
                 {
                     await this.ShowMessageAsync("", "Please select a Chair amount.");
                 }
+                else if (txtBleeds.Text == "")
+                {
+                    await this.ShowMessageAsync("", "Please select an estimated Bleed amount.");
+                }
                 else
                 {
-                    Selected.StartTime = cboStartTime.Text;
-                    Selected.EndTime = cboEndTime.Text;
-                    Selected.Length = CollectionManager.GetLength(cboStartTime.Text, cboEndTime.Text);
-                    Selected.Location = txtLocation.Text;
-                    Selected.MDC = cboMDC.Text;
+                    Selected.LOD = double.Parse(txtLOD.Text);
+                    Selected.Type = cboType.Text;
                     Selected.Chairs = int.Parse(cboChairs.Text);
+                    Selected.Bleeds = int.Parse(cboChairs.Text);
                     CollectionManager.UpdateSession(Selected);
                     this.DialogResult = true;
                 }
@@ -108,6 +98,14 @@ namespace ERSApp.Views
             {
                 await this.ShowMessageAsync("", "Please remove all staff before changing session details.");
             }
+        }
+
+        //Method to force only numbers in textbox input
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            //Use this in the xaml file to only allow numbers in input
+            Regex regex = new Regex("[^0-9.]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
