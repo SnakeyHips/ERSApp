f84f01911e08da44c987c7e465ed48b665fee988
Jake Gough <super.jake@hotmail.co.uk>
2018-05-25 09:40:36 +0100

    Update ArchiveStaffWindow.xaml.cs
    added create roster report functionality

--- Views/ArchiveStaffWindow.xaml.cs
+++ Views/ArchiveStaffWindow.xaml.cs
@@ -1,9 +1,17 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Collections.ObjectModel;
-using ERSApp.Model;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Win32;
+using ERSApp.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ERSApp.Views
 {
@@ -48,6 +56,132 @@ namespace ERSApp.Views
             CalculateDialog calculateDialog = new CalculateDialog();
             calculateDialog.Owner = this;
             calculateDialog.ShowDialog();
+        }
+
+        private async void btnReportStaff_Click(object sender, RoutedEventArgs e)
+        {
+            ArchiveReportDialog archiveReportDialog = new ArchiveReportDialog();
+            archiveReportDialog.DataContext = this;
+            archiveReportDialog.Owner = this;
+            archiveReportDialog.ShowDialog();
+            if(archiveReportDialog.DialogResult == true)
+            {
+                List<Staff> Rosters = new List<Staff>();
+                foreach (double week in archiveReportDialog.lstReportWeeks.SelectedItems)
+                {
+                    ObservableCollection<Staff> Temp = CollectionManager.GetRoster(week);
+                    foreach(Staff s in Temp)
+                    {
+                        bool match = false;
+                        foreach(Staff r in Rosters)
+                        {
+                            if(r.Id == s.Id)
+                            {
+                                match = true;
+                                r.ContractHours += s.ContractHours;
+                                r.AppointedHours += s.ContractHours;
+                                r.AbsenceHours += s.AbsenceHours;
+                                break;
+                            }
+                        }
+                        if(match == false)
+                        {
+                            Rosters.Add(s);
+                        }
+                    }
+                }
+                if (Rosters.Count() > 0)
+                {
+                    SaveFileDialog saveDialog = new SaveFileDialog();
+                    saveDialog.Title = "Choose Report Save Location";
+                    saveDialog.FileName = "Roster Report";
+                    saveDialog.Filter = "PDF document (*.pdf)|*.pdf";
+                    bool? result = saveDialog.ShowDialog();
+
+                    if (result == true)
+                    {
+                        //disable main window and activate progress ring while report is being created
+                        string start = archiveReportDialog.lstReportWeeks.SelectedItems[0].ToString();
+                        string end = archiveReportDialog.lstReportWeeks.SelectedItems[archiveReportDialog.lstReportWeeks.SelectedItems.Count-1].ToString();
+                        await CreateSessionReport(Rosters, start, end, saveDialog.FileName);
+                    }
+                }
+            }
+        }
+
+        //MEthod for creating sessions pdf report
+        private async Task CreateSessionReport(List<Staff> rosters, string start, string end, string path)
+        {
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None);
+
+                //Document is A4 size with margins of 36 each side
+                Document report = new Document(PageSize.A4, 10, 10, 10, 10);
+
+                PdfWriter writer = PdfWriter.GetInstance(report, fs);
+
+                //Table for displaying stock quantities with 2 being amount of columns
+                PdfPTable RosterTable = new PdfPTable(7);
+                RosterTable.SpacingBefore = 10f;
+                RosterTable.WidthPercentage = 100;
+
+                //Used for creating bold font
+                Font header = FontFactory.GetFont(FontFactory.HELVETICA_BOLD);
+                Font bold = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 8);
+                Font norm = FontFactory.GetFont(FontFactory.HELVETICA, 8);
+
+                //Column titles with bold text for stock table
+                RosterTable.AddCell(new Paragraph("Id", bold));
+                RosterTable.AddCell(new Paragraph("Name", bold));
+                RosterTable.AddCell(new Paragraph("Contract", bold));
+                RosterTable.AddCell(new Paragraph("Appointed", bold));
+                RosterTable.AddCell(new Paragraph("Absence", bold));
+                RosterTable.AddCell(new Paragraph("Neg", bold));
+                RosterTable.AddCell(new Paragraph("CO", bold));
+
+                foreach (Staff s in rosters)
+                {
+                    RosterTable.AddCell(new Paragraph(s.Id.ToString(), norm));
+                    RosterTable.AddCell(new Paragraph(s.Name, norm));
+                    RosterTable.AddCell(new Paragraph(s.ContractHours.ToString(), norm));
+                    RosterTable.AddCell(new Paragraph(s.AppointedHours.ToString(), norm));
+                    RosterTable.AddCell(new Paragraph(s.AbsenceHours.ToString(), norm));
+                    double difference = s.ContractHours - (s.AppointedHours + s.AbsenceHours);
+                    if(difference > 0)
+                    {
+                        RosterTable.AddCell(new Paragraph(difference.ToString(), norm));
+                        RosterTable.AddCell(new Paragraph("0", norm));
+                    }
+                    else if (difference < 0)
+                    {
+                        RosterTable.AddCell(new Paragraph("0", norm));
+                        RosterTable.AddCell(new Paragraph(Math.Abs(difference).ToString(), norm));
+                    }
+                    else
+                    {
+                        RosterTable.AddCell(new Paragraph("0", norm));
+                        RosterTable.AddCell(new Paragraph("0", norm));
+                    }
+                }
+
+                //Title used with date and time when created
+                Paragraph titleParagraph = new Paragraph("Roster Report: Week " + start + " - Week " + end, header);
+                titleParagraph.Alignment = Element.ALIGN_CENTER;
+
+                //Creates and adds everything to pdf output
+                report.Open();
+                report.Add(titleParagraph);
+                report.Add(RosterTable);
+                report.Close();
+
+                await this.ShowMessageAsync("", "Report created successfully!");
+            }
+            catch
+            {
+                //Most common issue for report not producing is that previous file is already open
+                await this.ShowMessageAsync("", "Report failed to create. Please make sure a report is not already open.");
+            }
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
