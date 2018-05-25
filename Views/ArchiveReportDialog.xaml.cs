4deaafcfec90afaf29f3188f14aec6d489b2745a
Jake Gough <super.jake@hotmail.co.uk>
2018-05-25 09:41:08 +0100

    Add files via upload
    uploaded archive report dialog files

---
+++ Views/ArchiveReportDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ERSApp.Views
+{
+    public partial class ArchiveReportDialog : MetroWindow
+    {
+        public ArchiveReportDialog()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstReportWeeks.SelectedItems != null)
+            {
+                DialogResult = true;
+            }
+            else
+            {
+                await this.ShowMessageAsync("", "Please select weeks to create report.");
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
