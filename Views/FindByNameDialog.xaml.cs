1b6fc8050f92c8441be7f1946fcbec02af497adf
Jake Gough <super.jake@hotmail.co.uk>
2018-05-25 10:24:04 +0100

    Add files via upload
    uploaded findbynamedialog for find staff by name functionality

---
+++ Views/FindByNameDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using ERSApp.Model;
+using ERSApp.ViewModel;
+using MahApps.Metro.Controls;
+
+namespace ERSApp.Views
+{
+    public partial class FindByNameDialog : MetroWindow
+    {
+        public FindByNameDialog(string name)
+        {
+            InitializeComponent();
+            foreach(Staff s in StaffViewModel.Staffs)
+            {
+                if (s.Name.Contains(name))
+                {
+                    lstNames.Items.Add(s);
+                }
+            }
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstNames.SelectedItem != null)
+            {
+                this.DialogResult = true;
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
