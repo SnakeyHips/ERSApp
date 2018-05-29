27fdc61411069fae4ec91e3bf9aa8ef50d31d517
Jake Gough <super.jake@hotmail.co.uk>
2018-05-29 11:28:14 +0100

    Add files via upload

---
+++ Views/AdminView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ERSApp.ViewModel;
+using MahApps.Metro.Controls;
+
+namespace ERSApp.Views
+{
+    public partial class AdminView : UserControl
+    {
+        MetroWindow mainWindow = (Application.Current.MainWindow as MetroWindow);
+
+        public AdminView()
+        {
+            InitializeComponent();
+            this.DataContext = new AdminViewModel();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+        }
+    }
+}
