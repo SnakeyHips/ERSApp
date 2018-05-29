609affbb7f1d02cb873b65d2ea2744d51245bb22
Jake Gough <super.jake@hotmail.co.uk>
2018-05-29 11:26:31 +0100

    Update SiteViewModel.cs
    put sql methods in here as should be

--- ViewModel/SiteViewModel.cs
+++ ViewModel/SiteViewModel.cs
@@ -1,23 +1,48 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Windows;
 using ERSApp.Model;
+using Dapper;
 
 namespace ERSApp.ViewModel
 {
     public class SiteViewModel
     {
+        public static string connString = ConfigurationManager.ConnectionStrings["ERSDBConnectionString"].ConnectionString;
+        public static ObservableCollection<Site> CommunityLocations { get; set; }
+        public static ObservableCollection<Site> MDCLocations { get; set; }
+        public static string SelectedLocation { get; set; }
+
         public SiteViewModel()
         {
             LoadLocations();
         }
 
-        public static ObservableCollection<Site> CommunityLocations { get; set; }
-        public static ObservableCollection<Site> MDCLocations { get; set; }
-        public static string SelectedLocation { get; set; }
-
         public static void LoadLocations()
         {
-            CommunityLocations = CollectionManager.GetSites("Community");
-            MDCLocations = CollectionManager.GetSites("MDC");
+            CommunityLocations = GetSitesType("Community");
+            MDCLocations = GetSitesType("MDC");
+        }
+
+        public static ObservableCollection<Site> GetSitesType(string type)
+        {
+            string query = "SELECT * FROM SiteTable Where Type=@Type";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    return new ObservableCollection<Site>(conn.Query<Site>(query, new { type }).ToList());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    return new ObservableCollection<Site>();
+                }
+            }
         }
     }
 }
