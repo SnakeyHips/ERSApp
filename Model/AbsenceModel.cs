5f902a6070a0b5e5e1cae4bba5aaa2c584a76780
Jake Gough <super.jake@hotmail.co.uk>
2018-05-29 09:14:00 +0100

    Update AbsenceModel.cs
    put change in wrong class oops

--- Model/AbsenceModel.cs
+++ Model/AbsenceModel.cs
@@ -1,26 +1,121 @@
-using ERSApp.Model;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
 
-namespace ERSApp.ViewModel
+namespace ERSApp.Model
 {
-    public class AbsenceViewModel
+    public class AbsenceModel
     {
-        public AbsenceViewModel()
-        {
-            LoadAbsences();
-        }
+    }
 
-        public static ObservableCollection<Absence> Absences { get; set; }
-        public static Absence SelectedAbsence { get; set; }
+    public class Absence : INotifyPropertyChanged
+    {
+        private int staffId;
+        private string staffName;
+        private string type;
+        private string startDate;
+        private string endDate;
+        private double length;
 
-        public static void LoadAbsences()
+        public int StaffId
         {
-            Absences = CollectionManager.GetAbsences();
-            foreach (Staff s in StaffViewModel.Staffs)
+            get
             {
-                s.Status = CollectionManager.GetStatus(s.Id, CollectionManager.SelectedDate);
+                return staffId;
+            }
+            set
+            {
+                if (staffId != value)
+                {
+                    staffId = value;
+                    RaisePropertyChanged("StaffId");
+                }
             }
         }
 
+        public string StaffName
+        {
+            get
+            {
+                return staffName;
+            }
+            set
+            {
+                if (staffName != value)
+                {
+                    staffName = value;
+                    RaisePropertyChanged("StaffName");
+                }
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (type != value)
+                {
+                    type = value;
+                    RaisePropertyChanged("Type");
+                }
+            }
+        }
+
+        public string StartDate
+        {
+            get
+            {
+                return startDate;
+            }
+            set
+            {
+                if (startDate != value)
+                {
+                    startDate = value;
+                    RaisePropertyChanged("StartDate");
+                }
+            }
+        }
+
+        public string EndDate
+        {
+            get
+            {
+                return endDate;
+            }
+            set
+            {
+                if (endDate != value)
+                {
+                    endDate = value;
+                    RaisePropertyChanged("EndDate");
+                }
+            }
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (length != value)
+                {
+                    length = value;
+                    RaisePropertyChanged("Length");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
