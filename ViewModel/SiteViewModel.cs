6ec22457eb319bea462dafc14fa7cc819b58852a
Jake Gough <super.jake@hotmail.co.uk>
2018-05-29 09:14:54 +0100

    Update SiteViewModel.cs
    used auto getset for selected

--- ViewModel/SiteViewModel.cs
+++ ViewModel/SiteViewModel.cs
@@ -12,26 +12,12 @@ namespace ERSApp.ViewModel
 
         public static ObservableCollection<Site> CommunityLocations { get; set; }
         public static ObservableCollection<Site> MDCLocations { get; set; }
+        public static string SelectedLocation { get; set; }
 
         public static void LoadLocations()
         {
             CommunityLocations = CollectionManager.GetSites("Community");
             MDCLocations = CollectionManager.GetSites("MDC");
-        }
-
-        private static string _selectedLocation;
-
-        public static string SelectedLocation
-        {
-            get
-            {
-                return _selectedLocation;
-            }
-
-            set
-            {
-                _selectedLocation = value;
-            }
         }
     }
 }
