26b32deb83952315cb3f19ef7a0301fbeadba8b5
Jake Gough <super.jake@hotmail.co.uk>
2018-05-31 11:58:00 +0100

    Add files via upload

---
+++ Views/HolidayView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ERSApp.ViewModel;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ERSApp.Views
+{
+    public partial class HolidayView : UserControl
+    {
+        MetroWindow mainWindow = (Application.Current.MainWindow as MetroWindow);
+
+        public HolidayView()
+        {
+            InitializeComponent();
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                this.DataContext = new AdminViewModel();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddHolidayWindow addHolidayWindow = new AddHolidayWindow();
+            addHolidayWindow.Owner = mainWindow;
+            addHolidayWindow.ShowDialog();
+        }
+
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateHolidayWindow updateHolidayWindow = new UpdateHolidayWindow(AdminViewModel.SelectedHoliday);
+            updateHolidayWindow.Owner = mainWindow;
+            updateHolidayWindow.ShowDialog();
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (AdminViewModel.SelectedHoliday != null)
+            {
+                MessageDialogResult choice = await mainWindow.ShowMessageAsync("",
+                        "Are you sure you want to delete this Holiday?",
+                        MessageDialogStyle.AffirmativeAndNegative);
+                if (choice == MessageDialogResult.Affirmative)
+                {
+                    AdminViewModel.DeleteHoliday(AdminViewModel.SelectedHoliday);
+                    AdminViewModel.Holidays.Remove(AdminViewModel.SelectedHoliday);
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+        }
+    }
+}
---
+++ Views/SiteView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ERSApp.ViewModel;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ERSApp.Views
+{
+    public partial class SiteView : UserControl
+    {
+        MetroWindow mainWindow = (Application.Current.MainWindow as MetroWindow);
+
+        public SiteView()
+        {
+            InitializeComponent();
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                this.DataContext = new AdminViewModel();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddSiteWindow addSiteWindow = new AddSiteWindow();
+            addSiteWindow.Owner = mainWindow;
+            addSiteWindow.ShowDialog();
+        }
+
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (AdminViewModel.SelectedSite != null)
+            {
+                UpdateSiteWindow updateSiteWindow = new UpdateSiteWindow(AdminViewModel.SelectedSite);
+                updateSiteWindow.Owner = mainWindow;
+                updateSiteWindow.ShowDialog();
+            }
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (AdminViewModel.SelectedSite != null)
+            {
+                MessageDialogResult choice = await mainWindow.ShowMessageAsync("",
+                            "Are you sure you want to delete this Site?",
+                            MessageDialogStyle.AffirmativeAndNegative);
+                if (choice == MessageDialogResult.Affirmative)
+                {
+                    AdminViewModel.DeleteSite(AdminViewModel.SelectedSite);
+                    AdminViewModel.Sites.Remove(AdminViewModel.SelectedSite);
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+        }
+    }
+}
