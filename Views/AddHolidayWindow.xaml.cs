ddc3ae57add904f23cf0f51fb8f13fdb6bd8ea8c
Jake Gough <super.jake@hotmail.co.uk>
2018-06-04 09:50:55 +0100

    Update AddHolidayWindow.xaml.cs

--- Views/AddHolidayWindow.xaml.cs
+++ Views/AddHolidayWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System.Windows;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
-using ERSApp.Model;
-using ERSApp.ViewModel;
+using ERSApp.Models;
+using ERSApp.ViewModels;
 
 namespace ERSApp.Views
 {
