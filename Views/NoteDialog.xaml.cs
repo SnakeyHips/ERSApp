fac7e187e8fbeb6ae48aca7277e3d86f21e09302
Jake Gough <super.jake@hotmail.co.uk>
2018-06-26 11:19:58 +0100

    Add files via upload

---
+++ Views/NoteDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace ERSApp.Views
+{
+    public partial class NoteDialog : MetroWindow
+    {
+        public NoteDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
