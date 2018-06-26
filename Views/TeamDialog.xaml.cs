afd75cfe2aac7ea56c741414e5a14e1ec4b773ee
Jake Gough <super.jake@hotmail.co.uk>
2018-06-26 08:47:46 +0100

    Add files via upload

---
+++ Views/TeamDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using ERSApp.ViewModels;
+
+namespace ERSApp.Views
+{
+    public partial class TeamDialog : MetroWindow
+    {
+        public TeamDialog()
+        {
+            InitializeComponent();
+            lstTeamDialog.ItemsSource = TeamViewModel.Teams;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstTeamDialog.SelectedIndex > -1)
+            {
+                this.DialogResult = true;
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
