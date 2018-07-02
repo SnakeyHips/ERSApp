87b1cb617fe53645413db2e49b58c0ee28bcb046
Jake Gough <super.jake@hotmail.co.uk>
2018-07-02 11:19:03 +0100

    Add files via upload

---
+++ Models/SkillModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+
+namespace ERSApp.Models
+{
+    public class SkillModel
+    {
+    }
+
+    public class Skill : INotifyPropertyChanged
+    {
+        private string role;
+        private string name;
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+            set
+            {
+                if (role != value)
+                {
+                    role = value;
+                    RaisePropertyChanged("Role");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
