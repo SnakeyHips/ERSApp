0ff3d835f6b8e90b02fdd018dae03893f2110295
Jake Gough <super.jake@hotmail.co.uk>
2018-07-02 11:21:02 +0100

    Add files via upload

---
+++ Views/AddSkillWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using ERSApp.Models;
+using ERSApp.ViewModels;
+
+namespace ERSApp.Views
+{
+    public partial class AddSkillWindow : MetroWindow
+    {
+        public AddSkillWindow()
+        {
+            InitializeComponent();
+            cboRole.Items.Add("SV");
+            cboRole.Items.Add("DRI");
+            cboRole.Items.Add("RN");
+            cboRole.Items.Add("CCA");
+        }
+
+        private async void btnAddSkill_Click(object sender, RoutedEventArgs e)
+        {
+            if (cboRole.Text == "")
+            {
+                await this.ShowMessageAsync("", "Please enter a Role.");
+            }
+            else if (txtName.Text == "")
+            {
+                await this.ShowMessageAsync("", "Please enter a Skill Name.");
+            }
+            else
+            {
+                Skill temp = new Skill()
+                {
+                    Role = cboRole.Text,
+                    Name = txtName.Text
+                };
+                AdminViewModel.AddSkill(temp);
+                AdminViewModel.Skills.Add(temp);
+                this.DialogResult = true;
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
---
+++ Views/SkillView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ERSApp.ViewModels;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ERSApp.Views
+{
+    public partial class SkillView : UserControl
+    {
+        MetroWindow mainWindow = (Application.Current.MainWindow as MetroWindow);
+
+        public SkillView()
+        {
+            InitializeComponent();
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                this.DataContext = new AdminViewModel();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddSkillWindow addSkillWindow = new AddSkillWindow();
+            addSkillWindow.Owner = mainWindow;
+            addSkillWindow.ShowDialog();
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (AdminViewModel.SelectedSkill != null)
+            {
+                MessageDialogResult choice = await mainWindow.ShowMessageAsync("",
+                        "Are you sure you want to delete this Skill?",
+                        MessageDialogStyle.AffirmativeAndNegative);
+                if (choice == MessageDialogResult.Affirmative)
+                {
+                    AdminViewModel.DeleteSkill(AdminViewModel.SelectedSkill);
+                    AdminViewModel.Skills.Remove(AdminViewModel.SelectedSkill);
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Close();
+        }
+    }
+}
